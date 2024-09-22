7c57d7f4bb56c28ceda01d50d4afb3b932ef0d6c
santanunath <43264004+santanunath@users.noreply.github.com>
2024-09-22 18:14:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+import Link from 'next/link'
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    
+    <div className=''>
+      Homepage
+      <br />
+      <br />
+      <Link href="/sign-in">Sign-in page</Link>
+      <br />
+      <Link href="/admin">Admin Dashboard</Link>
+      <br />
+      <Link href="/student">Student Dashboard</Link>
+      <br />
+      <Link href="/parent">Parent Dashboard</Link>
+      <br />
+      <Link href="/teacher">Teacher Dashboard</Link>
+    </div>
   )
 }
 
